451019540ba46d65b9c14dc65caaac5637bdfacc
ta1355 <eorbs255@naver.com>
2025-07-12 05:40:16 +0900

    first commit

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+    public float jumpForce = 7f;
+
+    private Rigidbody2D rb;
+
+    private bool isGrounded = false;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        HandleMovement();
+        HandleJump();
+    }
+
+    void HandleMovement()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    void HandleJump()
+    {
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.contacts[0].normal.y > 0.7f)
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+    }
+}
